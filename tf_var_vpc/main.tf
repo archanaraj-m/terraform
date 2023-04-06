ce884c4d805d1a2bc92e45a2d7bdd555afeebf5a
Archana <archana.garrepalli533@gmail.com>
2023-04-06 18:13:42 +0530

    add subnet file and vpc var

--- tf_var_vpc/main.tf
+++ tf_var_vpc/main.tf
@@ -1,39 +1,39 @@
 
 resource "aws_vpc" "ntier" {
-    cidr_block = var.ntier-vpc-range
-    tags = {
-        Name = "ntier"
-    }
-}    
+  cidr_block = var.ntier-vpc-range
+  tags = {
+    Name = "ntier"
+  }
+}
 resource "aws_subnet" "subnet1" {
-    cidr_block = var.ntier-subnet1-cidr
-    availability_zone = "${var.region}a"
-    vpc_id = aws_vpc.ntier.id
-    depends_on = [
-        aws_vpc.ntier
-    ]
+  cidr_block        = var.ntier-subnet1-cidr
+  availability_zone = "${var.region}a"
+  vpc_id            = aws_vpc.ntier.id
+  depends_on = [
+    aws_vpc.ntier
+  ]
 }
 resource "aws_subnet" "subnet2" {
-    cidr_block = var.ntier-subnet2-cidr
-    availability_zone = "${var.region}b"
-    vpc_id = aws_vpc.ntier.id
-    depends_on = [
-        aws_vpc.ntier
-    ]
+  cidr_block        = var.ntier-subnet2-cidr
+  availability_zone = "${var.region}b"
+  vpc_id            = aws_vpc.ntier.id
+  depends_on = [
+    aws_vpc.ntier
+  ]
 }
-resource "aws_subnet" "db11" {
-    cidr_block = var.ntier-db1-cidr
-    availability_zone = "${var.region}a"
-    vpc_id = aws_vpc.ntier.id
-    depends_on = [
-        aws_vpc.ntier
-    ]
+resource "aws_subnet" "db1" {
+  cidr_block        = var.ntier-db1-cidr
+  availability_zone = "${var.region}a"
+  vpc_id            = aws_vpc.ntier.id
+  depends_on = [
+    aws_vpc.ntier
+  ]
 }
 resource "aws_subnet" "db2" {
-    cidr_block = var.ntier-db2-cidr
-    availability_zone = "${var.region}b"
-    vpc_id = aws_vpc.ntier.id
-    depends_on = [
-        aws_vpc.ntier
-    ]
+  cidr_block        = var.ntier-db2-cidr
+  availability_zone = "${var.region}b"
+  vpc_id            = aws_vpc.ntier.id
+  depends_on = [
+    aws_vpc.ntier
+  ]
 }
--- tf_var_vpc/values.tfvars
+++ tf_var_vpc/values.tfvars
@@ -1,3 +1,3 @@
-region = "eu-west-3"
-ntier-vpc-range = "10.100.0.0/16"
+region             = "eu-west-3"
+ntier-vpc-range    = "10.100.0.0/16"
 ntier-subnet1-cidr = "10.100.0.0/24"
