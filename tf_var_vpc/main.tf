e16e8fcd5dddcf094e1dc7faa46dd1b945c4819c
Archana <archana.garrepalli533@gmail.com>
2023-04-06 16:10:31 +0530

    add changes

--- tf_var_vpc/main.tf
+++ tf_var_vpc/main.tf
@@ -1,18 +1,6 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = ">= 4.47.0"
-    }
-  }
-}
 
-
-provider "aws" {
-}
 resource "aws_vpc" "ntier" {
-    cidr_block = "ntier-vpc-range"
+    cidr_block = var.ntier-vpc-range
     tags = {
         Name = "ntier"
     }
